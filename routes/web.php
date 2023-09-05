2098901933b0d94fa35dafa5f8be203fb70a5ab5
Pramudya Eko Niti Taruno <nititaruno@gmail.com>
2023-09-05 12:04:03 +0800

    view akun

--- routes/web.php
+++ routes/web.php
@@ -32,8 +32,3 @@ Route::get('/pengelola', [PersonController::class, 'index'])->name('pengelola');
 Route::get('/usaha', [UsahaController::class, 'index'])->name('usaha');
 // });
 
-
-Route::get('/test', function() {
-    return view('pages.akun.index', [
-        'akun' => Person::paginate(5)]);
-});
