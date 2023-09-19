365158f39469287fef4eae24dcedf6e1a1182c02
Pramudya Eko Niti Taruno <nititaruno@gmail.com>
2023-09-19 16:04:31 +0800

    dashboard

--- resources/views/pages/dashboard/index.blade.php
+++ resources/views/pages/dashboard/index.blade.php
@@ -7,15 +7,91 @@
 @section('common_content')
     <h2 class="text-2xl font-medium mb-5 pb-1 flex flex-inline border-b-4 border-cyan-500">Dashboard</h2>
 
+    <div class="grid grid-cols-5 gap-2 pt-1 mb-3">
+
+        <div class="bg-neutral-100 rounded-lg p-4 flex">
+            <div class="pr-2"> 
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#155E75" class="w-10 h-12">
+                    <path d="M4.5 3.75a3 3 0 00-3 3v.75h21v-.75a3 3 0 00-3-3h-15z" />
+                    <path fill-rule="evenodd" d="M22.5 9.75h-21v7.5a3 3 0 003 3h15a3 3 0 003-3v-7.5zm-18 3.75a.75.75 0 01.75-.75h6a.75.75 0 010 1.5h-6a.75.75 0 01-.75-.75zm.75 2.25a.75.75 0 000 1.5h3a.75.75 0 000-1.5h-3z" clip-rule="evenodd" />
+                  </svg>
+                  
+            </div>
+            <div>
+                <p class="text-base">Hutang</p>
+                <p class="text-base font-semibold">Rp. 100000000</p>
+            </div> 
+        </div>
+
+        <div class="bg-neutral-100 rounded-lg p-4 flex">
+            <div class="pr-2"> 
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#155E75" class="w-10 h-12">>
+                    <path d="M21 6.375c0 2.692-4.03 4.875-9 4.875S3 9.067 3 6.375 7.03 1.5 12 1.5s9 2.183 9 4.875z" />
+                    <path d="M12 12.75c2.685 0 5.19-.586 7.078-1.609a8.283 8.283 0 001.897-1.384c.016.121.025.244.025.368C21 12.817 16.97 15 12 15s-9-2.183-9-4.875c0-.124.009-.247.025-.368a8.285 8.285 0 001.897 1.384C6.809 12.164 9.315 12.75 12 12.75z" />
+                    <path d="M12 16.5c2.685 0 5.19-.586 7.078-1.609a8.282 8.282 0 001.897-1.384c.016.121.025.244.025.368 0 2.692-4.03 4.875-9 4.875s-9-2.183-9-4.875c0-.124.009-.247.025-.368a8.284 8.284 0 001.897 1.384C6.809 15.914 9.315 16.5 12 16.5z" />
+                    <path d="M12 20.25c2.685 0 5.19-.586 7.078-1.609a8.282 8.282 0 001.897-1.384c.016.121.025.244.025.368 0 2.692-4.03 4.875-9 4.875s-9-2.183-9-4.875c0-.124.009-.247.025-.368a8.284 8.284 0 001.897 1.384C6.809 19.664 9.315 20.25 12 20.25z" />
+                  </svg>                  
+            </div>
+            <div>
+                <p class="text-base">Piutang</p>
+                <p class="text-base font-semibold">Rp. 0</p>
+            </div> 
+        </div>
+
+        <div class="bg-neutral-100 rounded-lg p-4 flex">
+            <div class="pr-2"> 
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="#155E75" class="w-10 h-12">>
+                    <path fill-rule="evenodd" d="M9.293 2.293a1 1 0 011.414 0l7 7A1 1 0 0117 11h-1v6a1 1 0 01-1 1h-2a1 1 0 01-1-1v-3a1 1 0 00-1-1H9a1 1 0 00-1 1v3a1 1 0 01-1 1H5a1 1 0 01-1-1v-6H3a1 1 0 01-.707-1.707l7-7z" clip-rule="evenodd" />
+                  </svg>
+                  
+                  
+                  
+            </div>
+            <div>
+                <p class="text-base">Biaya Jasa</p>
+                <p class="text-base font-semibold">Rp. 0</p>
+            </div> 
+        </div>
+       
+
+        <div class="bg-neutral-100 rounded-lg p-4 flex">
+            <div class="pr-2"> 
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#155E75" class="w-10 h-12">>
+                    <path d="M5.223 2.25c-.497 0-.974.198-1.325.55l-1.3 1.298A3.75 3.75 0 007.5 9.75c.627.47 1.406.75 2.25.75.844 0 1.624-.28 2.25-.75.626.47 1.406.75 2.25.75.844 0 1.623-.28 2.25-.75a3.75 3.75 0 004.902-5.652l-1.3-1.299a1.875 1.875 0 00-1.325-.549H5.223z" />
+                    <path fill-rule="evenodd" d="M3 20.25v-8.755c1.42.674 3.08.673 4.5 0A5.234 5.234 0 009.75 12c.804 0 1.568-.182 2.25-.506a5.234 5.234 0 002.25.506c.804 0 1.567-.182 2.25-.506 1.42.674 3.08.675 4.5.001v8.755h.75a.75.75 0 010 1.5H2.25a.75.75 0 010-1.5H3zm3-6a.75.75 0 01.75-.75h3a.75.75 0 01.75.75v3a.75.75 0 01-.75.75h-3a.75.75 0 01-.75-.75v-3zm8.25-.75a.75.75 0 00-.75.75v5.25c0 .414.336.75.75.75h3a.75.75 0 00.75-.75v-5.25a.75.75 0 00-.75-.75h-3z" clip-rule="evenodd" />
+                  </svg>
+     
+            </div>
+            <div>
+                <p class="text-base">Biaya Dagang</p>
+                <p class="text-base font-semibold">Rp. 0</p>
+            </div> 
+        </div>
+
+        <div class="bg-neutral-100 rounded-lg p-4 flex">
+            <div class="pr-2"> 
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="#155E75" class="w-10 h-12">>
+                    <path fill-rule="evenodd" d="M4.5 2A1.5 1.5 0 003 3.5v13A1.5 1.5 0 004.5 18h11a1.5 1.5 0 001.5-1.5V7.621a1.5 1.5 0 00-.44-1.06l-4.12-4.122A1.5 1.5 0 0011.378 2H4.5zm2.25 8.5a.75.75 0 000 1.5h6.5a.75.75 0 000-1.5h-6.5zm0 3a.75.75 0 000 1.5h6.5a.75.75 0 000-1.5h-6.5z" clip-rule="evenodd" />
+                  </svg>
+                  
+                  
+                  
+                  
+            </div>
+            <div>
+                <p class="text-base">Total Lainnya</p>
+                <p class="text-base font-semibold">Rp. 0</p>
+            </div> 
+        </div>
+    </div>
+
+
     <div class="flex">
 
         {{-- Line Chart --}}
         <div class="basis-1/2 bg-neutral-100 rounded-lg p-6">
             <div>
-                <h2 class="text-xl font-medium mb-5 pb-1 flex flex-inline border-cyan-500">Grafik Kas/Pendapatan</h2>
-
-                
-                <p>Rentang Data yang ditampilkan: 17 Agustus 2020 - 19 April 2023</p>
+                <h2 class="text-xl font-medium mb-5 pb-4 flex flex-inline border-cyan-500">Grafik Kas/Pendapatan</h2>
                 <canvas id="myLineChart" class="mt-4"></canvas>
                 
                 <div class="flex mt-4 items-center" >
@@ -39,7 +115,7 @@
 
         {{-- Tabel  --}}
         <div class="basis-1/2 ml-4 bg-neutral-100 rounded-lg p-6">
-            <h2 class="text-xl font-medium mb-5 pb-1 flex flex-inline border-cyan-500">Tabel Apalah</h2>
+            <h2 class="text-xl font-medium mb-5 pb-1 flex flex-inline border-cyan-500">Usaha Aktif</h2>
             <table class="w-full text-sm text-left text-gray-500">
             <thead class="text-xs text-gray-700 border-gray-400 uppercase bg-gray-50">
                 <tr>
@@ -83,29 +159,7 @@
         
 
     </div>
-    <div class="grid grid-cols-5 gap-4 pt-4">
-
-        <div class="bg-neutral-100 rounded-lg p-4 text-center">
-            <p class="text-gray-900">Hutang Bumdes</p>
-            <p class="font-medium text-gray-900"><span>Rp.</span>100000</p>
-        </div>
-        <div class="bg-neutral-100 rounded-lg p-4">
-            <p>Total Biaya Jasa</p>
-            <p>10000</p>
-        </div>
-        <div class="bg-neutral-100 rounded-lg p-4">
-            <p>Total Transaksi Lainnya</p>
-            <p>10000</p>
-        </div>
-        <div class="bg-neutral-100 rounded-lg p-4">
-            <p>Piutang</p>
-            <p>10000</p>
-        </div>
-        <div class="bg-neutral-100 rounded-lg p-4">
-            <p>Total Biaya dagang</p>
-            <p>10000</p>
-        </div>
-    </div>
+    
 
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
             <script>
