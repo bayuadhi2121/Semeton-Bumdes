778fb61e720c2b113c2ac9b50823073d5f6b2056
BayuAdhipramana <wbayuadhi2121@gmail.com>
2023-11-01 21:25:09 +0800

    Update JurnalUmum.php

--- app/Livewire/JurnalUmum.php
+++ app/Livewire/JurnalUmum.php
@@ -15,13 +15,14 @@ class JurnalUmum extends Component
             ->join('akuns', 'akuns.id_akun', '=', 'jurnal_umums.id_akun')
             ->join('transaksis', 'transaksis.id_transaksi', '=', 'jurnal_umums.id_transaksi')
             ->join('usahas', 'usahas.id_usaha', '=', 'transaksis.id_usaha');
+
         if (Auth::user()->status == 'Bendahara') {
             $result->where('usahas.id_person', null);
         } elseif (Auth::user()->status == 'Akuntan') {
             $result->where('usahas.id_person', Auth::user()->id_person);
         }
-        $result->select('akuns.*', 'jurnal_umums.*')
-            ->get();
+
+        $result = $result->select('akuns.*', 'jurnal_umums.*')->get();
         // dd($result);
         return view('livewire.jurnal-umum.jurnal-umum', [
             'jurnal' => $result
