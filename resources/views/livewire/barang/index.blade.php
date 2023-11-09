fc18e79fe2004c28a4ac0518039e46c9a49cf7d5
BayuAdhipramana <wbayuadhi2121@gmail.com>
2023-11-09 16:17:40 +0800

    Update index.blade.php

--- resources/views/livewire/barang/index.blade.php
+++ resources/views/livewire/barang/index.blade.php
@@ -10,7 +10,6 @@
             <input type="text" id="simple-search" wire:model.live='search'
                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-cyan-500 focus:border-cyan-500 block w-full p-2.5"
                 placeholder="Cari Barang..." required>
-            @endcanw
             @if ($search != '')
             <button wire:click="resetSearch()"
                 class="text-gray-500 absolute right-2 bottom-4 font-medium rounded-lg text-sm">
