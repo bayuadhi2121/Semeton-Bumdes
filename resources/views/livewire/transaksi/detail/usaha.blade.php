69c71086d3eb62898ce64b49f793a18a88cdfdf7
BayuAdhipramana <wbayuadhi2121@gmail.com>
2023-11-10 17:14:54 +0800

    Update usaha.blade.php

--- resources/views/livewire/transaksi/detail/usaha.blade.php
+++ resources/views/livewire/transaksi/detail/usaha.blade.php
@@ -101,7 +101,7 @@
             <label class="text-gray-800 font-semibold basis-4/12 ">
                 Sisa
             </label>
-            <input type="text" id="small-input" wire:model.live='sisa'
+            <input type="text" id="small-input" wire:model='sisa'
                 class="sisa basis-2/5 bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-cyan-500 focus:border-cyan-500 block w-full p-2.5 mr-2"
                 disabled>
             @if (!$transaksi->saved)
