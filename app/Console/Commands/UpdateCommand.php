dc718d9f837e62c3ba3228de5cf48cb624390418
BayuAdhipramana <anggrainisri225@gmail.com>
2024-01-05 15:54:51 +0800

    Update UpdateCommand.php

--- app/Console/Commands/UpdateCommand.php
+++ app/Console/Commands/UpdateCommand.php
@@ -57,5 +57,30 @@ class UpdateCommand extends Command
             ->where('usahas.status', 'Jasa')
             ->groupBy('jurnal_umums.id_akun', 'akuns.nama')
             ->get();
+        $dagang =
+            JurnalUmum::join('transaksis', 'jurnal_umums.id_transaksi', '=', 'transaksis.id_transaksi')
+            ->join('akuns', 'jurnal_umums.id_akun', '=', 'akuns.id_akun')
+            ->join('usahas', 'usahas.id_usaha', '=', 'transaksis.id_usaha')
+            ->join('jual_belis', 'transaksis.id_transaksi', '=', 'jual_belis.id_transaksi')
+            ->join('jbdagangs', 'jual_belis.id_jualbeli', '=', 'jbdagangs.id_jualbeli')
+            ->join('barangs', 'barangs.id_barang', '=', 'jbdagangs.id_barang')
+            ->select('usahas.nama')
+            ->selectRaw('SUM(CASE WHEN akuns.nama LIKE "%Penjualan%" THEN jurnal_umums.debit + jurnal_umums.kredit ELSE 0 END) AS penjualan')
+            ->selectRaw('SUM(CASE WHEN akuns.nama LIKE "%Pembelian%" THEN jurnal_umums.debit + jurnal_umums.kredit ELSE 0 END) AS pembelian')
+            ->selectRaw('SUM(CASE WHEN akuns.nama LIKE "%Pembelian%" THEN (barangs.harga+barangs.untung)*barangs.stok ELSE 0 END) AS total_jual')
+            ->whereBetween('transaksis.tanggal', [$start, $end])
+            ->where('akuns.nama', 'LIKE', '%Penjualan%')
+            ->orWhere('akuns.nama', 'LIKE', '%Pembelian%')
+            ->groupBy('usahas.nama')
+            ->get();
+        $beban = JurnalUmum::join('akuns', 'jurnal_umums.id_akun', '=', 'akuns.id_akun')
+            ->join('transaksis', 'jurnal_umums.id_transaksi', '=', 'transaksis.id_transaksi')
+            ->join('usahas', 'akuns.id_usaha', '=', 'usahas.id_usaha')
+            ->select('jurnal_umums.id_akun', 'akuns.nama')
+            ->selectRaw('SUM(jurnal_umums.debit + jurnal_umums.kredit) as total')
+            ->whereBetween('transaksis.tanggal', [$start, $end])
+            ->where('akuns.nama', 'LIKE', '%Biaya%')
+            ->groupBy('jurnal_umums.id_akun', 'akuns.nama')
+            ->get();
     }
 }
